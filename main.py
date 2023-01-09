ab50d606eccbc338ec22187f493de950b0747a02
Abubakkar Khan <sittiksimano@gmail.com>
2023-01-10 05:21:11 +0600

    Update main.py
    Its in testing phase, May not work, Don't use unit final announcement 📢  done..

--- main.py
+++ main.py
@@ -1,35 +1,80 @@
 import os
-os.system("rm -rf cloudflare")
+import subprocess
+import shutil
+import argparse
+import logging
 
-os.system("clear")
+def run_command(command):
+    """Run a shell command and return the output.
+    If the command fails, raise a CalledProcessError.
+    """
+    output = subprocess.check_output(command, shell=True)
+    return output.decode('utf-8').strip()
 
-os.system("apt install wget php -y") 
+def check_command(command):
+    """Check if a command is available on the system.
+    """
+    return shutil.which(command) is not None
 
-os.system("pkg install proot -y")
-os.system("pkg install proot resolv-conf -y")
-os.system("mkdir cloudflare")
+def install_package(package):
+    """Install a package using apt.
+    """
+    run_command(f"apt install {package} -y")
 
-sysInfo = os.popen("uname -m")
-sysData = sysInfo.read().replace("\n","")
+def setup_cloudflare(arch, output_dir):
+    """Set up Cloudflare for the specified architecture.
+    """
+    if arch == "aarch64" or arch == "Android":
+        url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm"
+    elif arch == "arm64":
+        url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64"
+    elif arch == "x86_64":
+        url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64"
+    else:
+        url = "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386"
 
-if sysData=="aarch64" or sysData =="Android":
-     os.system("cd cloudflare && wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm")
-elif sysData=="arm64":
-     os.system("cd cloudflare && wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64")
-elif sysData =="x86_64":
-     os.system("cd cloudflare && wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64")
-else :
-     os.system("cd cloudflare && wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386")
+    run_command(f"wget {url} -O {output_dir}/cloudflared")
+    run_command(f"chmod +x {output_dir}/cloudflared")
+    run_command(f"cp {output_dir}/cloudflared $PREFIX/bin/cloudflared")
+    run_command("chmod +X $PREFIX/bin/cloudflared")
 
-os.system("cd cloudflare && chmod +x *")
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--arch", type=str, default="", help="target architecture (aarch64, arm64, x86_64, or 386)")
+    parser.add_argument("-o", "--output", type=str, default=".", help="output directory")
+    args = parser.parse_args()
 
-getFileby = os.popen("ls cloudflare")
-getFilename = getFileby.read().replace("\n","")
-os.system(f"cd cloudflare && mv {getFilename} cldf")
-os.system("cd cloudflare && cp cldf $PREFIX/bin/")
-os.system("chmod +X $PREFIX/bin/cldf")
-os.system("rm -rf cloudflare")
+    arch = args.arch
+    output_dir = args.output
 
-os.system("clear")
-print (" [*] Setup Done :)")
-print (" Run :: cldf --help ")
+    # Set up logging
+    logging.basicConfig(level=logging.INFO)
+
+    # Check if required commands are available
+    if not check_command("apt"):
+        logging.error("apt is not available on this system.")
+        return
+    if not check_command("wget"):
+        logging.info("wget is not available, installing it...")
+        install_package("wget")
+    if not check_command("php"):
+        logging.info("php is not available, installing it...")
+        install_package("php")
+    if not check_command("proot"):
+        logging.info("proot is not available, installing it...")
+        install_package("proot")
+      # Determine the target architecture
+    if not arch:
+        arch_info = run_command("uname -m")
+        arch = arch_info.strip()
+
+    # Set up Cloudflare
+    try:
+        setup_cloudflare(arch, output_dir)
+        logging.info("Cloudflare setup complete.")
+    except subprocess.CalledProcessError as e:
+        logging.error("Error setting up Cloudflare: %s", e)
+
+if __name__ == "__main__":
+    main()
